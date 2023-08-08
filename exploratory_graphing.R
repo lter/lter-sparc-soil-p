e87bfa162fb84c58106aca97617c0aa83bdcb847
angelchen7 <angelchen7777@yahoo.com>
2023-08-08 16:21:03 -0700

    adding more content to the visualization section

--- exploratory_graphing.R
+++ exploratory_graphing.R
@@ -15,7 +15,6 @@
 librarian::shelf(tidyverse, googledrive)
 
 # Create necessary sub-folder(s)
-dir.create(path = file.path("exploratory_graphs"), showWarnings = F)
 dir.create(path = file.path("tidy_data"), showWarnings = F)
 
 # Identify megadata data file
@@ -35,12 +34,40 @@ rm(list = ls())
 ## ------------------------------------------ ##
 
 # Read in megadata file
-megadata <- read.csv(file.path("tidy_data", "tidy_soil_p.csv"))
+megadata <- read.csv(file.path("tidy_data", "tidy_soil_p.csv")) %>%
+  # Filter out the rows with no treatment info
+  dplyr::filter(nchar(treatment) != 0)
 
 # Check columns
 dplyr::glimpse(megadata)
 
-# tinker
-ggplot(data = megadata, aes(x = total_P_absolute, y = C_stock_mg_m2, color = treatment)) +
-  geom_point(show.legend = F) +
-  facet_grid(rows = vars(site))
+# Create necessary sub-folder(s) to export our visualizations in
+# Customize as needed, for example I made a sub-folder to store all the slow P vs. N conc graphs
+dir.create(path = file.path("exploratory_graphs"), showWarnings = F)
+dir.create(path = file.path("exploratory_graphs", "slowP_Nconc"), showWarnings = F)
+
+# Point to our export folders
+slowP_Nconc_folder <- file.path("exploratory_graphs", "slowP_Nconc")
+
+for (a_site in unique(megadata$dataset)){
+  # Filter our megadata to only one site
+  example_site <- megadata %>%
+    dplyr::filter(dataset == a_site)
+  
+  # Saving the exploratory plot as a png
+  # Edit the file path as needed
+  png(filename = file.path(slowP_Nconc_folder, paste0(a_site, ".png")), width = 850, height = 850, units = "px")
+  
+  # Creating the exploratory plot
+  # Edit the x and y values as needed
+  example_plot <- ggplot(data = example_site, aes(x = total_P_mg_kg, y = N_conc_percent, color = treatment)) +
+    geom_point(show.legend = T) +
+    facet_grid(site ~ ., scales = "free") 
+  
+  # Plotting it
+  plot(example_plot)
+  
+  dev.off()
+}
+
+
