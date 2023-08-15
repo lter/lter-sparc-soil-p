192b05a0fd80cc43855576d25a47ae0ab9b3106e
njlyon0 <nicholasjlyon@gmail.com>
2023-08-15 11:45:02 -0400

    Tweaked plots so user can specify X/Y axes as objects and re-run the plot code without changing anything. Also assembled file name from user variable choices so each plot has an accurate / flexible name

--- exploratory_graphing.R
+++ exploratory_graphing.R
@@ -16,6 +16,7 @@ librarian::shelf(tidyverse, googledrive)
 
 # Create necessary sub-folder(s)
 dir.create(path = file.path("tidy_data"), showWarnings = F)
+dir.create(path = file.path("exploratory_graphs"), showWarnings = F)
 
 # Identify megadata data file
 tidy_id <- googledrive::drive_ls(googledrive::as_id("https://drive.google.com/drive/u/0/folders/1pjgN-wRlec65NDLBvryibifyx6k9Iqy9"), type = "csv") %>%
@@ -30,7 +31,7 @@ purrr::walk2(.x = tidy_id$id, .y = tidy_id$name,
 rm(list = ls())
 
 ## ------------------------------------------ ##
-#             Visualization ----
+#                 Data Prep ----
 ## ------------------------------------------ ##
 
 # Read in megadata file
@@ -43,14 +44,27 @@ megadata <- read.csv(file.path("tidy_data", "tidy_soil_p.csv")) %>%
 # Check columns
 dplyr::glimpse(megadata[1:30])
 
-## Exploring across-site ------------------------- ##
+## ------------------------------------------ ##
+#         Cross-LTER Visualization ----
+## ------------------------------------------ ##
 
-# Create necessary sub-folder(s) to export our visualizations in
-dir.create(path = file.path("exploratory_graphs"), showWarnings = F)
+# Choose an X variable and Y variable for across-site plotting
+xsite_xvar <- "total_P_mg_kg"
+xsite_yvar <- "N_conc_percent"
+
+# Check that they are both in the data
+## X
+if(xsite_xvar %in% names(megadata)){
+  message("X variable looks good!")
+} else { message("X variable not found in 'megadata'. Check spelling") }
+## Y
+if(xsite_yvar %in% names(megadata)){
+  message("Y variable looks good!")
+} else { message("Y variable not found in 'megadata'. Check spelling") }
 
 # Creating the exploratory plot
 # Edit the x and y values as needed
-ggplot(data = megadata, aes(x = total_P_mg_kg, y = N_conc_percent, color = lter)) +
+ggplot(data = megadata, aes(x = .data[[xsite_xvar]], y = .data[[xsite_yvar]], color = lter)) +
   geom_point() +
   # Best-fit line by site
   geom_smooth(method = "lm", formula = "y ~ x", alpha = 0.2, show.legend = F) +
@@ -58,11 +72,28 @@ ggplot(data = megadata, aes(x = total_P_mg_kg, y = N_conc_percent, color = lter)
   geom_smooth(color = "black",fill = "gray82", method = "lm", formula = "y ~ x") +
   theme_bw()
 
+# Assemble a descriptive name for this file
+xsite_filename <- paste0("overall_", xsite_xvar, "_vs_", xsite_yvar, ".png")
+
 # Export the plot if you want
-ggsave(filename = file.path("exploratory_graphs", "overall_totalP_Nconc.png"),
+ggsave(filename = file.path("exploratory_graphs", xsite_filename),
        plot = last_plot(), width = 6, height = 6, units = "in")
 
 ## Exploring site + treatment combinations ------- ##
+
+# Choose within-LTER x and y axes
+within_site_xvar <- "slow_P_mg_kg"
+within_site_yvar <- "C_conc_percent"
+
+# Check they are in the data too
+## X
+if(within_site_xvar %in% names(megadata)){
+  message("X variable looks good!")
+} else { message("X variable not found in 'megadata'. Check spelling") }
+## Y
+if(within_site_yvar %in% names(megadata)){
+  message("Y variable looks good!")
+} else { message("Y variable not found in 'megadata'. Check spelling") }
 
 # Loop across LTERs
 for (single_lter in unique(megadata$lter)){
@@ -70,6 +101,11 @@ for (single_lter in unique(megadata$lter)){
   # Filter our megadata to only one subdataset
   example_site <- dplyr::filter(megadata, lter == single_lter)
   
+  # Only make a plot if there are some values in this subset
+  ## Translation: 'if not all of X and not all of Y are missing values, then make plot'
+  if(!all(is.na(example_site[[within_site_xvar]])) & 
+     !all(is.na(example_site[[within_site_yvar]]))){
+    
   # Create a graph
   ggplot(data = example_site, aes(x = slow_P_mg_kg, y = C_conc_percent, color = lter)) +
     geom_point() +
@@ -80,10 +116,13 @@ for (single_lter in unique(megadata$lter)){
     theme_bw()
   
   # Make a name for this graph
-  example_name <- paste0(single_lter, "_slowP_Cconc.png")
-  
+  within_site_filename <- paste0(single_lter, "_", within_site_xvar, "_vs_", 
+                                 within_site_yvar, ".png")
+    
   # Export the graph
-  ggsave(filename = file.path("exploratory_graphs", example_name),
+  ggsave(filename = file.path("exploratory_graphs", within_site_filename),
          plot = last_plot(), width = 6, height = 6, units = "in")
   
-}
+  } # Close conditional that checked for non-NA values
+  
+} # Close across LTER loop
