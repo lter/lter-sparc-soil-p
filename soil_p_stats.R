dcc45c7d1d51acaa6b13df878a90099f7004384e
njlyon0 <nicholasjlyon@gmail.com>
2023-08-25 14:34:15 -0400

    First pass at filtering megadata to only shallowest core of mineral horizon per site

--- soil_p_stats.R
+++ soil_p_stats.R
@@ -13,7 +13,7 @@
 
 # Load necessary libraries
 # install.packages("librarian")
-librarian::shelf(tidyverse, googledrive)
+librarian::shelf(tidyverse, googledrive, supportR)
 
 # Create necessary sub-folder(s)
 dir.create(path = file.path("tidy_data"), showWarnings = F)
@@ -36,9 +36,37 @@ mega <- read.csv(file.path("tidy_data", "tidy_soil_p.csv"))
 # Check structure
 dplyr::glimpse(mega)
 
+# Depth subsetting is used to restrict core depth of samples used in analysis
+## Decide how many centimeters from the first measured depth (of the A horizon) are allowed
+depth_cutoff <- 15
+
 # Megadata includes *a lot* of information and we only really need a subset of it for stats
+stat_df <- mega %>%
+  # Pare down to only columns of interest
+  ## Unspecified columns are implicitly removed
+  dplyr::select(lter, dataset, site, plot, block, core, dplyr::starts_with("treatment"),
+                dplyr::starts_with("horizon_"), dplyr::starts_with("depth_"),
+                core_length_cm, pH:soil_mass_g_m2,
+                dplyr::starts_with("N_"), dplyr::starts_with("C_")) %>%
+  # Only interested in mineral layer
+  ## Assuming that un-specified horizons are mineral layer
+  dplyr::filter(horizon_binary == "mineral" | nchar(horizon_binary) == 0) %>%
+  # Identify minimum depth of remaining data within sample info column groups
+  dplyr::group_by(dplyr::across(c(lter:treatment_years))) %>%
+  dplyr::mutate(min_depth = ifelse(!all(is.na(depth_start_cm)),
+                                   yes = min(depth_start_cm, na.rm = T),
+                                   no = NA),
+                max_allowed_depth = (min_depth + depth_cutoff),
+                .after = horizon_binary) %>%
+  dplyr::ungroup() %>%
+  # Filter to only samples in that range
+  dplyr::filter(depth_start_cm >= min_depth & depth_end_cm <= max_allowed_depth)
 
+# Check out the structure of the data
+dplyr::glimpse(stat_df)
 
+# Check to make sure we're okay with the columns we dropped
+supportR::diff_check(old = names(mega), new = names(stat_df), sort = F)
 
 
 # End ----
