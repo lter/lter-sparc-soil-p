45d446632d8f2e5b6107c12c679321deeda66853
njlyon0 <nicholasjlyon@gmail.com>
2023-09-07 14:55:49 -0400

    Finished updating column names throughout entire script with new data key format / column anatomy. Commented-out slow P calculation because need full methods info to know what column names will be

--- soil_p_wrangle.R
+++ soil_p_wrangle.R
@@ -1137,61 +1137,66 @@ dplyr::glimpse(tidy_v10)
 ## ------------------------------------------ ##
 
 # Glimpse the entire dataset
-dplyr::glimpse(tidy_v7)
+dplyr::glimpse(tidy_v10)
 
 # Now we'll want to add together our various types of P (conditionally)
-p_sums <- tidy_v7 %>%
+p_sums <- tidy_v10 %>%
   # First need to fill NAs with 0s to avoid making NA sums
   ## Pivot longer
-  tidyr::pivot_longer(cols = -lter:-C_conc_org_percent,
+  tidyr::pivot_longer(cols = c(dplyr::starts_with("P_"),
+                               dplyr::starts_with("Po_"),
+                               dplyr::starts_with("ReBHsin_"),
+                               dplyr::starts_with("Pi_")),
                       names_to = "names", values_to = "values") %>%
   ## Remove NA / missing values
   dplyr::filter(!is.na(values) & nchar(values) != 0) %>%
   ## Pivot back to wide format and fill empty cells with 0
   tidyr::pivot_wider(names_from = names, values_from = values, values_fill = 0) %>%
   # Calculate slow P conditionally
-  dplyr::mutate(slow_P_mg_kg = dplyr::case_when(
-    dataset == "HJAndrews_1" ~ (P_conc_HCl_mg_kg),
-    dataset == "Bonanza Creek_1" ~ NA,
-    dataset == "Bonanza Creek_2" ~ NA,
-    dataset == "Brazil" ~ NA,
-    dataset == "Calhoun" ~ (P_conc_HCl_mg_kg),
-    # (vvv) Data seem to only have total P but searching for Hedley fraction
-    dataset == "CedarCreek_1" ~ NA, 
-    dataset == "Coweeta" ~ (P_conc_HCl_mg_kg), 
-    # (vvv) Only have a neutral salt extraction (available P). May remove dataset entirely
-    dataset == "Fernow" ~ NA,
-    dataset == "FloridaCoastal" ~ (P_conc_HCl_mg_kg),
-    dataset == "Hubbard Brook" ~ (P_conc_HNO3_cold_mg_kg),
-    dataset == "Jornada" ~ NA,
-    dataset == "Kellog_Biological_Station" ~ (Pi_conc_HCl_mg_kg),
-    # (vvv) Based on 2 different methodologies
-    dataset == "Konza_1" ~ (P_conc_Ca_bound_mg_kg),
-    dataset == "Luquillo_1" ~ NA,
-    # (vvv) Re-check! assuming these are the correct units, and dilute HCl is 1M step from Hedley
-    dataset == "Luquillo_2" ~ (P_conc_dilHCl_mg_kg),
-    dataset == "Niwot_1" ~ (P_conc_HCl_mg_kg),
-    dataset == "Niwot_2" ~ (P_conc_HCl_mg_kg),
-    dataset == "Niwot_3" ~ NA,
-    dataset == "Sevilleta_1" ~ (P_conc_HCl_mg_kg),
-    dataset == "Sevilleta_2" ~ NA,
-    # (vvv) If resulting number is negative it gets set to zero
-    dataset == "Toolik" ~ ifelse((P_conc_HCl_mg_kg - P_conc_citrate_mg_kg) < 0,
-                                 yes = 0, 
-                                 no = P_conc_HCl_mg_kg - P_conc_citrate_mg_kg), 
-    TRUE ~ NA )) %>%
+  ## NOTE: using placeholder (obviously wrong) while we await complete methods data knowledge / inclusion in data key
+  dplyr::mutate(slow.P_mg.kg = 1) %>%
+  # dplyr::mutate(slow.P_mg_kg = dplyr::case_when(
+  #   dataset == "HJAndrews_1" ~ (P_conc_HCl_mg_kg),
+  #   dataset == "Bonanza Creek_1" ~ NA,
+  #   dataset == "Bonanza Creek_2" ~ NA,
+  #   dataset == "Brazil" ~ NA,
+  #   dataset == "Calhoun" ~ (P_conc_HCl_mg_kg),
+  #   # (vvv) Data seem to only have total P but searching for Hedley fraction
+  #   dataset == "CedarCreek_1" ~ NA, 
+  #   dataset == "Coweeta" ~ (P_conc_HCl_mg_kg), 
+  #   # (vvv) Only have a neutral salt extraction (available P). May remove dataset entirely
+  #   dataset == "Fernow" ~ NA,
+  #   dataset == "FloridaCoastal" ~ (P_conc_HCl_mg_kg),
+  #   dataset == "Hubbard Brook" ~ (P_conc_HNO3_cold_mg_kg),
+  #   dataset == "Jornada" ~ NA,
+  #   dataset == "Kellog_Biological_Station" ~ (Pi_conc_HCl_mg_kg),
+  #   # (vvv) Based on 2 different methodologies
+  #   dataset == "Konza_1" ~ (P_conc_Ca_bound_mg_kg),
+  #   dataset == "Luquillo_1" ~ NA,
+  #   # (vvv) Re-check! assuming these are the correct units, and dilute HCl is 1M step from Hedley
+  #   dataset == "Luquillo_2" ~ (P_conc_dilHCl_mg_kg),
+  #   dataset == "Niwot_1" ~ (P_conc_HCl_mg_kg),
+  #   dataset == "Niwot_2" ~ (P_conc_HCl_mg_kg),
+  #   dataset == "Niwot_3" ~ NA,
+  #   dataset == "Sevilleta_1" ~ (P_conc_HCl_mg_kg),
+  #   dataset == "Sevilleta_2" ~ NA,
+  #   # (vvv) If resulting number is negative it gets set to zero
+  #   dataset == "Toolik" ~ ifelse((P_conc_HCl_mg_kg - P_conc_citrate_mg_kg) < 0,
+  #                                yes = 0, 
+  #                                no = P_conc_HCl_mg_kg - P_conc_citrate_mg_kg), 
+  #   TRUE ~ NA )) %>%
   # Also total P
   ## NOTE: using slow P as placeholder while we wait for identification of P fractions / site
-  dplyr::mutate(total_P_mg_kg = slow_P_mg_kg) %>%
+  dplyr::mutate(total.P_mg.kg = slow.P_mg.kg) %>%
   # After summing, remove all columns where we changed NAs to 0s
-  dplyr::select(lter:C_conc_org_percent, slow_P_mg_kg, total_P_mg_kg) %>%
+  dplyr::select(lter:soil.mass_g.m2, slow.P_mg.kg, total.P_mg.kg) %>%
   # Keep only unique rows
   dplyr::distinct()
 
 # Any datasets missing?
 p_sums %>%
-  dplyr::filter(is.na(slow_P_mg_kg) | is.na(total_P_mg_kg)) %>%
-  dplyr::select(dataset, slow_P_mg_kg, total_P_mg_kg) %>%
+  dplyr::filter(is.na(slow.P_mg.kg) | is.na(total.P_mg.kg)) %>%
+  dplyr::select(dataset, slow.P_mg.kg, total.P_mg.kg) %>%
   dplyr::distinct()
 
 # Check structure
@@ -1201,41 +1206,40 @@ dplyr::glimpse(p_sums)
 ## They're not "real" 0s so we want to preserve the real 0s while still easily getting sums
 
 # Now we can attach our sums to the original tidy object
-tidy_v8 <- tidy_v7 %>%
+tidy_v11 <- tidy_v10 %>%
   # By not specifying which columns to join by, all shared columns will be used
   dplyr::left_join(y = p_sums) %>%
   # Move our P sums to the left for more easy reference
-  dplyr::relocate(slow_P_mg_kg, total_P_mg_kg, .after = bulk_density_kg_ha)
+  dplyr::relocate(slow.P_mg.kg, total.P_mg.kg, .after = bulk.density_kg.ha)
 
 # Check structure
-dplyr::glimpse(tidy_v8)
+dplyr::glimpse(tidy_v11)
 
 ## ------------------------------------------ ##
               # Absolute P ----
 ## ------------------------------------------ ##
 
 # Calculate absolute P totals (rather than portions of each core)
-tidy_v9 <- tidy_v8 %>%
+tidy_v12 <- tidy_v11 %>%
   # Due to our earlier depth/bulk density prep this is easy!
-  dplyr::mutate(slow_P_stock = slow_P_mg_kg * core_length_cm * bulk_density_g_cm3,
-                .before = slow_P_mg_kg) %>%
-  dplyr::mutate(total_P_stock = total_P_mg_kg * core_length_cm * bulk_density_g_cm3,
-                .before = total_P_mg_kg)
+  dplyr::mutate(slow.P_stock = slow.P_mg.kg * core.length_cm * bulk.density_g.cm3,
+                .before = slow.P_mg.kg) %>%
+  dplyr::mutate(total.P_stock = total.P_mg.kg * core.length_cm * bulk.density_g.cm3,
+                .before = total.P_mg.kg)
 
 # Check contents of those columns
-summary(tidy_v9$slow_P_stock)
-summary(tidy_v9$total_P_stock)
+summary(tidy_v12$slow.P_stock)
+summary(tidy_v12$total.P_stock)
 
 # Re-check structure
-dplyr::glimpse(tidy_v9[1:35])
-
+dplyr::glimpse(tidy_v12[1:35])
 
 ## ------------------------------------------ ##
                   # Export ----
 ## ------------------------------------------ ##
 
 # Create a final data object
-final_tidy <- tidy_v10
+final_tidy <- tidy_v12
 
 # Check its structure
 dplyr::glimpse(final_tidy)
@@ -1243,16 +1247,26 @@ dplyr::glimpse(final_tidy)
 # Create a folder to export into
 dir.create(path = file.path("tidy_data"), showWarnings = F)
 
+# Define the tidy file name
+tidy_name <- "tidy_soil_p.csv"
+
 # Save out the final data object
-write.csv(x = final_tidy, file = file.path("tidy_data", "tidy_soil_p.csv"), 
+write.csv(x = final_tidy, file = file.path("tidy_data", tidy_name), 
           row.names = F, na = "")
 
 # Upload to GoogleDrive
-googledrive::drive_upload(media = file.path("tidy_data", "tidy_soil_p.csv"), overwrite = T,
+googledrive::drive_upload(media = file.path("tidy_data", tidy_name), overwrite = T,
                           path = googledrive::as_id("https://drive.google.com/drive/u/0/folders/1pjgN-wRlec65NDLBvryibifyx6k9Iqy9"))
+
+# Also define a name for the archival variant of the data
+arch_name <- "sparc-soil-p_archival-data.csv"
 
 # Also export the version of the data for archiving in a data repository
 write.csv(x = archive, row.names = F, na = '',
-          file = file.path("tidy_data", "sparc_soil-p-synthesis-data.csv"))
+          file = file.path("tidy_data", arch_name))
+
+# Upload that as well
+googledrive::drive_upload(media = file.path("tidy_data", arch_name), overwrite = T,
+                          path = googledrive::as_id("https://drive.google.com/drive/u/0/folders/1pjgN-wRlec65NDLBvryibifyx6k9Iqy9"))
 
 # End ----
