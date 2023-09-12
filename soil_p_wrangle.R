e1e4a14cc6b9532319bb3ac074330f46467d77c2
njlyon0 <nicholasjlyon@gmail.com>
2023-09-12 13:05:53 -0400

    Renamed objects to avoid confusion with 'harmonizing' script and exported to Drive

--- soil_p_wrangle.R
+++ soil_p_wrangle.R
@@ -1,5 +1,5 @@
 ## ------------------------------------------ ##
-        # SPARC Soil P -- Data Wrangling
+       # SPARC Soil P -- Data Wrangling
 ## ------------------------------------------ ##
 # Script author(s): Nick J Lyon
 
@@ -74,10 +74,10 @@ dplyr::glimpse(sparc_v2)
 ## ------------------------------------------ ##
 
 # Glimpse the entire dataset
-dplyr::glimpse(tidy_v10)
+dplyr::glimpse(sparc_v2)
 
 # Now we'll want to add together our various types of P (conditionally)
-p_sums <- tidy_v10 %>%
+p_sums <- sparc_v2 %>%
   # First need to fill NAs with 0s to avoid making NA sums
   ## Pivot longer
   tidyr::pivot_longer(cols = c(dplyr::starts_with("P_"),
@@ -143,70 +143,58 @@ dplyr::glimpse(p_sums)
 ## They're not "real" 0s so we want to preserve the real 0s while still easily getting sums
 
 # Now we can attach our sums to the original tidy object
-tidy_v11 <- tidy_v10 %>%
+sparc_v3 <- sparc_v2 %>%
   # By not specifying which columns to join by, all shared columns will be used
   dplyr::left_join(y = p_sums) %>%
   # Move our P sums to the left for more easy reference
   dplyr::relocate(slow.P_conc_mg.kg, total.P_conc_mg.kg, .after = bulk.density_kg.ha)
 
 # Check structure
-dplyr::glimpse(tidy_v11)
+dplyr::glimpse(sparc_v3)
 
 ## ------------------------------------------ ##
-# Stock P Calculations ----
+          # Stock P Calculations ----
 ## ------------------------------------------ ##
 
 # Calculate absolute P totals (rather than portions of each core)
-tidy_v12 <- tidy_v11 %>%
-  # Due to our earlier depth/bulk density prep this is easy!
-  dplyr::mutate(slow.P_stock_g.m2 = ((slow.P_conc_mg.kg * core.length_cm * bulk.density_g.cm3) * 10^4) / 10^6,
+sparc_v4 <- sparc_v3 %>%
+  # Multiply P concentration by core length & bulk density to get stocks
+  dplyr::mutate(slow.P_temp = slow.P_conc_mg.kg * core.length_cm * bulk.density_g.cm3,
+                total.P_temp = total.P_conc_mg.kg * core.length_cm * bulk.density_g.cm3) %>%
+  # Do unit conversions to get to g/m2
+  dplyr::mutate(slow.P_stock_g.m2 = (slow.P_temp * 10^4) / 10^6,
                 .before = slow.P_conc_mg.kg) %>%
-  dplyr::mutate(total.P_stock_g.m2 = ((total.P_conc_mg.kg * core.length_cm * bulk.density_g.cm3) * 10^4) / 10^6,
-                .before = total.P_conc_mg.kg)
+  dplyr::mutate(total.P_stock_g.m2 = (total.P_temp * 10^4) / 10^6,
+                .before = total.P_conc_mg.kg) %>%
+  # Drop intermediary columns
+  dplyr::select(-slow.P_temp, -total.P_temp)
 
 # Check contents of those columns
-summary(tidy_v12$slow.P_stock)
-summary(tidy_v12$total.P_stock)
+summary(sparc_v4$slow.P_stock_g.m2)
+summary(sparc_v4$total.P_stock_g.m2)
 
 # Re-check structure
-dplyr::glimpse(tidy_v12[1:35])
+dplyr::glimpse(sparc_v4[1:35])
 
 ## ------------------------------------------ ##
-# Export ----
+        # Export SPARC-Ready Data ----
 ## ------------------------------------------ ##
 
 # Create a final data object
-final_tidy <- tidy_v12
+final_sparc <- sparc_v4
 
 # Check its structure
-dplyr::glimpse(final_tidy)
-
-# Create a folder to export into
-dir.create(path = file.path("tidy_data"), showWarnings = F)
-
-# And identify the tidy data Drive URL
-tidy_drive <- googledrive::as_id("https://drive.google.com/drive/u/0/folders/1pjgN-wRlec65NDLBvryibifyx6k9Iqy9")
+dplyr::glimpse(final_sparc)
 
 # Define the tidy file name
 tidy_name <- "tidy_soil_p.csv"
 
 # Save out the final data object
-write.csv(x = final_tidy, file = file.path("tidy_data", tidy_name), 
+write.csv(x = final_sparc, file = file.path("tidy_data", tidy_name), 
           row.names = F, na = "")
 
 # Upload to GoogleDrive
 googledrive::drive_upload(media = file.path("tidy_data", tidy_name), 
-                          overwrite = T, path = tidy_drive)
-
-# Also define a name for the archival variant of the data
-arch_name <- "sparc-soil-p_archival-data.csv"
-
-# Export the version of the data for archiving in a data repository
-write.csv(x = archive, row.names = F, na = '',
-          file = file.path("tidy_data", arch_name))
-
-# Upload that as well
-googledrive::drive_upload(media = file.path("tidy_data", arch_name), 
                           overwrite = T, path = tidy_drive)
 
 # End ----
