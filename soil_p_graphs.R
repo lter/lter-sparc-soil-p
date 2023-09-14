4943a76a6a4ee079a8b60a70be0713a19458bc41
njlyon0 <nicholasjlyon@gmail.com>
2023-09-14 10:27:06 -0400

    First pass at site-average graphs

--- soil_p_graphs.R
+++ soil_p_graphs.R
@@ -19,13 +19,13 @@ librarian::shelf(tidyverse, googledrive, supportR)
 dir.create(path = file.path("tidy_data"), showWarnings = F)
 dir.create(path = file.path("graphs"), showWarnings = F)
 
-# Clear environment
-rm(list = ls())
-
 # Identify and download the tidied megadata object from the Drive
 googledrive::drive_ls(googledrive::as_id("https://drive.google.com/drive/u/0/folders/1pjgN-wRlec65NDLBvryibifyx6k9Iqy9")) %>%
   dplyr::filter(name == "tidy_soil_p.csv") %>%
   googledrive::drive_download(file = .$id, path = file.path("tidy_data", .$name), overwrite = T)
+
+# Clear environment
+rm(list = ls())
 
 ## ------------------------------------------ ##
             # Pre-Viz Wrangling ----
@@ -96,21 +96,83 @@ site_avgs <- main_df %>%
 dplyr::glimpse(site_avgs)
 
 ## ------------------------------------------ ##
+# Graph Housekeeping ----
+## ------------------------------------------ ##
 
+# Define some useful graph aesthetic components
+n_color1 <- "#f4a261"
+n_color2 <- "#e76f51"
+c_color1 <- "#2a9d8f"
+c_color2 <- "#264653"
+
+# Custom ggplot theme
+sparc_theme <- theme(panel.grid = element_blank(),
+                     panel.background = element_blank(),
+                     legend.position = "none",
+                     axis.line = element_line(color = "black"))
+
+## ------------------------------------------ ##
+          # Site Average Graphs ----
+## ------------------------------------------ ##
+
+# Make a N ~ total P graph
+ggplot(data = site_avgs, aes(x = total.P_conc_mg.kg, y = N_conc_percent)) +
+  geom_smooth(method = "lm", formula = "y ~ x", se = F, color = "black") +
+  geom_point(aes(fill = 'x'), pch = 21, size = 3) +
+  # Custom labels / colors / theme
+  labs(x = "Total P Conc. (mg/kg)", y = "N Conc. (%)") +
+  scale_fill_manual(values = n_color1) +
+  sparc_theme
+
+# Export it
+ggsave(filename = file.path("graphs", "nitrogen_total-P.png"),
+       width = 5, height = 5, units = "in")
+
+# Do the same for N ~ slow P
+ggplot(data = site_avgs, aes(x = slow.P_conc_mg.kg, y = N_conc_percent)) +
+  geom_smooth(method = "lm", formula = "y ~ x", se = F, color = "black") +
+  geom_point(aes(fill = 'x'), pch = 21, size = 3) +
+  # Custom labels / colors / theme
+  labs(x = "Slow P Conc. (mg/kg)", y = "N Conc. (%)") +
+  scale_fill_manual(values = n_color2) +
+  sparc_theme
+
+# Export it
+ggsave(filename = file.path("graphs", "nitrogen_slow-P.png"),
+       width = 5, height = 5, units = "in")
+
+# Make a C ~ total P graph
+ggplot(data = site_avgs, aes(x = total.P_conc_mg.kg, y = C_conc_percent)) +
+  geom_smooth(method = "lm", formula = "y ~ x", se = F, color = "black") +
+  geom_point(aes(fill = 'x'), pch = 21, size = 3) +
+  # Custom labels / colors / theme
+  labs(x = "Total P Conc. (mg/kg)", y = "C Conc. (%)") +
+  scale_fill_manual(values = c_color1) +
+  sparc_theme
+
+# Export it
+ggsave(filename = file.path("graphs", "carbon_total-P.png"),
+       width = 5, height = 5, units = "in")
+
+# Do the same for C ~ slow P
+ggplot(data = site_avgs, aes(x = slow.P_conc_mg.kg, y = C_conc_percent)) +
+  geom_smooth(method = "lm", formula = "y ~ x", se = F, color = "black") +
+  geom_point(aes(fill = 'x'), pch = 21, size = 3) +
+  # Custom labels / colors / theme
+  labs(x = "Slow P Conc. (mg/kg)", y = "N Conc. (%)") +
+  scale_fill_manual(values = c_color2) +
+  sparc_theme
+
+# Export it
+ggsave(filename = file.path("graphs", "carbon_slow-P.png"),
+       width = 5, height = 5, units = "in")
+
+## ------------------------------------------ ##
+# Site Average Combo Graph ----
 ## ------------------------------------------ ##
 
 
 
-## ------------------------------------------ ##
-# Site Average Graphs ----
-## ------------------------------------------ ##
-
-
-
-xsite_N_totP_fit <- lm(N_conc_percent ~ total.P_conc_mg.kg, data = site_avgs)
-xsite_C_totP_fit <- lm(C_conc_percent ~ total.P_conc_mg.kg, data = site_avgs)
-xsite_N_slowP_fit <- lm(N_conc_percent ~ slow.P_conc_mg.kg, data = site_avgs)
-xsite_C_slowP_fit <- lm(C_conc_percent ~ slow.P_conc_mg.kg, data = site_avgs)
 
 
 ## ------------------------------------------ ##
