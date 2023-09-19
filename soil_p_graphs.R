bc228e4be281cc86c64003d8a3af5c7dd5ccb8be
njlyon0 <nicholasjlyon@gmail.com>
2023-09-19 13:53:32 -0400

    First pass at custom function for across-site graphs

--- soil_p_graphs.R
+++ soil_p_graphs.R
@@ -83,6 +83,41 @@ sparc_theme <- theme(panel.grid = element_blank(),
 ### 1. Centralize plotting aesthetics / structure
 ### 2. Avoid re-typing code for every graph
 
+avg_graph <- function(data = avgs_df, x_var, y_var, 
+                      text_nudge_x = -0.1, text_nudge_y = 0.05){
+  
+  # Error out if X axis isn't in data
+  if(!x_var %in% names(data))
+    stop("X variable not found in data. Check spelling")
+  
+  # Do the same for Y axis
+  if(!y_var %in% names(data))
+    stop("Y variable not found in data. Check spelling")
+  
+  # Deduce the names of the standard error columns
+  x_se <- gsub(pattern = "mean", replacement = "std.error", x = x_var)
+  y_se <- gsub(pattern = "mean", replacement = "std.error", x = y_var)
+  
+  # Generate plot
+  q <- ggplot(data = data, aes(x = .data[[x_var]], y = .data[[y_var]])) +
+    # Y-axis error bars
+    geom_errorbar(aes(ymax = .data[[y_var]] + .data[[y_se]],
+                      ymin = .data[[y_var]] - .data[[y_se]])) +
+    # X-axis error bars
+    geom_errorbarh(aes(xmax = .data[[x_var]] + .data[[x_se]],
+                       xmin = .data[[x_var]] - .data[[x_se]])) +
+    # Best fit line
+    geom_smooth(method = "lm", formula = "y ~ x", se = F, color = "black") +
+    # Points/labels for each dataset
+    geom_point(aes(fill = lter), pch = 21, size = 3) +
+    geom_text(aes(label = dataset), nudge_y = text_nudge_y, nudge_x = text_nudge_x) +
+    # scale_fill_manual(values = n_color1) +
+    sparc_theme +
+    theme(legend.position = "none")
+  
+  # Return that plot
+  return(q) }
+
 
 
 
@@ -90,27 +125,13 @@ sparc_theme <- theme(panel.grid = element_blank(),
           # Site Average Graphs ----
 ## ------------------------------------------ ##
 
+# N % ~ total P
+avg_graph(x_var = "mean_total.P_conc_mg.kg", y_var = "mean_N_conc_percent") +
+  labs(x = "Mean Total P (mg/kg) ± SE", y = "Mean N (%) ± SE")
 
-
-
-ggplot(data = avgs_df, aes(x = mean_total.P_conc_mg.kg, y = mean_N_conc_percent)) +
-  # Y-axis error bars
-  geom_errorbar(aes(ymax = mean_N_conc_percent + std.error_N_conc_percent,
-                    ymin = mean_N_conc_percent - std.error_N_conc_percent)) +
-  # X-axis error bars
-  geom_errorbarh(aes(xmax = mean_total.P_conc_mg.kg + std.error_total.P_conc_mg.kg,
-                     xmin = mean_total.P_conc_mg.kg - std.error_total.P_conc_mg.kg)) +
-  # Best fit line
-  geom_smooth(method = "lm", formula = "y ~ x", se = F, color = "black") +
-  # Points/labels for each dataset
-  geom_point(aes(fill = dataset), pch = 21, size = 3) +
-  # geom_label(aes(label = dataset), position = position_dodge(width = 0.5)) +
-  geom_text(aes(label = dataset), nudge_y = 0.05, nudge_x = -0.1) +
-  # Custom labels / colors / theme
-  labs(x = "Mean Total P (mg/kg) ± SE", y = "Mean N (%) ± SE") +
-  # scale_fill_manual(values = n_color1) +
-  sparc_theme +
-  theme(legend.position = "none")
+# N % ~ slow P
+avg_graph(x_var = "mean_slow.P_conc_mg.kg", y_var = "mean_N_conc_percent") +
+  labs(x = "Mean Slow P (mg/kg) ± SE", y = "Mean N (%) ± SE")
 
 
 # Make a N ~ total P graph
