f7089689eeb2307388674f359a6ac8a48ebbdfe4
njlyon0 <nicholasjlyon@gmail.com>
2023-09-22 11:22:16 -0400

    Testing a possible improvement to the pre-plotting data wrangling code

--- soil_p_graphs.R
+++ soil_p_graphs.R
@@ -37,6 +37,9 @@ rm(list = ls())
 
 # Read in site averages data file
 site_df <- read.csv(file.path("tidy_data", "site-avgs_tidy-soil-p.csv")) %>%
+  # Fill missing SD/SE with 0 for plotting convenience
+  dplyr::mutate(dplyr::across(.cols = dplyr::starts_with("std."),
+                              .fns = ~ ifelse(is.na(.), yes = 0, no = .))) %>%
   # Get a column that is the dataset number
   tidyr::separate_wider_delim(cols = dataset_simp, delim = "_", too_few = "align_start",
                               names = c("dataset_dup", "dataset_num"), cols_remove = F) %>%
@@ -48,11 +51,22 @@ site_df <- read.csv(file.path("tidy_data", "site-avgs_tidy-soil-p.csv")) %>%
 # Check structure
 dplyr::glimpse(site_df)
 
+# Make one version each for N and C plotting respectively
+site_n <- dplyr::filter(.data = site_df, !is.na(mean_N_conc_percent))
+site_c <- dplyr::filter(.data = site_df, !is.na(mean_C_conc_percent))
+
 # Read in more granular (spatially) data
-plot_df <- read.csv(file.path("tidy_data", "plot-avgs_tidy-soil-p.csv"))
+plot_df <- read.csv(file.path("tidy_data", "plot-avgs_tidy-soil-p.csv")) %>%
+  # Fill missing SD/SE with 0 for plotting convenience
+  dplyr::mutate(dplyr::across(.cols = dplyr::starts_with("std."),
+                              .fns = ~ ifelse(is.na(.), yes = 0, no = .)))
 
 # Check structure
 dplyr::glimpse(plot_df)
+
+# Make one version each for N and C plotting respectively
+plot_n <- dplyr::filter(.data = plot_df, !is.na(mean_N_conc_percent))
+plot_c <- dplyr::filter(.data = plot_df, !is.na(mean_C_conc_percent))
 
 ## ------------------------------------------ ##
             # Graph Housekeeping ----
@@ -91,7 +105,7 @@ sparc_theme <- theme(panel.grid = element_blank(),
 dplyr::glimpse(site_df)
 
 # Early version of graph
-xsite_ntotp <- ggplot(data = site_df, aes(x = mean_total.P_conc_mg.kg, y = mean_N_conc_percent)) +
+xsite_ntotp <- ggplot(data = site_n, aes(x = mean_total.P_conc_mg.kg, y = mean_N_conc_percent)) +
   # Y-axis error bars
   geom_errorbar(aes(ymax = mean_N_conc_percent + std.error_N_conc_percent,
                     ymin = mean_N_conc_percent - std.error_N_conc_percent),
