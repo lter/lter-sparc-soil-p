49bf2ec8481c3a0738a005f052bb8f1a9ced2ac1
njlyon0 <nicholasjlyon@gmail.com>
2023-09-27 15:55:17 -0700

    Tweaked an object name (minor/cosmetic change)

--- 1_harmonize.R
+++ 1_harmonize.R
@@ -329,7 +329,7 @@ sort(unique(tidy_v1$distance))
 sort(unique(tidy_v1$topography))
 
 # Fix any typos identified above
-tidy_v2a <- tidy_v1 %>%
+tidy_v1b <- tidy_v1 %>%
   # Fix some of the spatial/site columns
   dplyr::mutate(lat = as.numeric(lat),
                 lon = as.numeric(lon),
@@ -404,15 +404,15 @@ tidy_v2a <- tidy_v1 %>%
                                     x = dataset_simp))
 
 # Check out new LTER column
-sort(unique(tidy_v2a$lter))
-sort(unique(tidy_v2a$dataset_simp))
+sort(unique(tidy_v1b$lter))
+sort(unique(tidy_v1b$dataset_simp))
 
 # Not all datasets are collected at the same level of spatial granularity
 ## Those that don't have a given level (e.g., data only at plot level not specific cores)...
 ## ...have NA in the levels of information that they are missing
 
 # If any piece of information is missing, fill with the next coarser piece
-tidy_v2b <- tidy_v2a %>%
+tidy_v2 <- tidy_v1b %>%
   # If site is missing, fill with dataset name
   dplyr::mutate(site = ifelse(test = (is.na(site) | nchar(site) == 0),
                               yes = dataset, no = site)) %>%
@@ -427,12 +427,12 @@ tidy_v2b <- tidy_v2a %>%
                               yes = plot, no = core))
 
 # Re-check structure
-tidy_v2b %>%
+tidy_v2 %>%
   dplyr::select(lter, dataset, site, block, plot, core) %>%
   dplyr::glimpse()
 
 # Collapse spatial organization to get a quick sense of how many granularity is available
-tidy_v2b %>%
+tidy_v2 %>%
   dplyr::group_by(lter, dataset) %>%
   dplyr::summarize(site_ct = length(unique(site)),
                    sites = paste(unique(site), collapse = "; "),
@@ -444,7 +444,7 @@ tidy_v2b %>%
                    cores = paste(unique(core), collapse = "; "))
 
 # Check structure
-dplyr::glimpse(tidy_v2b)
+dplyr::glimpse(tidy_v2)
 
 ## ------------------------------------------ ##
           # Depth & Horizon Fixes ----
