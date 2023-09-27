0de6d79ed6e03836e0115d686b938d2da41ed9ea
njlyon0 <nicholasjlyon@gmail.com>
2023-09-27 16:57:57 -0700

    Created working loop for averaging *any* stats-ready file

--- 5_averages.R
+++ 5_averages.R
@@ -49,34 +49,120 @@ for(focal_ready in local_files){
   # Starting message
   message("Beginning averaging steps for '", focal_ready, "'")
   
+  # Read in file
+  focal_df <- read.csv(file = file.path("data", "stats_ready", focal_ready))
   
+  # Prepare to do averaging steps flexibly
+  avgs_prep <- focal_df %>%
+    # Move site information columns to left and all together
+    dplyr::relocate(lter:raw_filename, site:core, 
+                    .before = dplyr::everything()) %>%
+    # Drop core-specific depth/horizon info
+    dplyr::select(-dplyr::starts_with("horizon"),
+                  -dplyr::starts_with("depth."), 
+                  -core.length_cm, -bulk.density_g.cm3) %>%
+    # Reshape to long format so all number columns are together
+    tidyr::pivot_longer(cols = -lter:-core,
+                        names_to = "variables", values_to = "vals")
   
-}
+  # Begin with averaging across cores within plots
+  plots_v1 <- avgs_prep %>%
+    # Group by everything and average the response variables
+    dplyr::group_by(lter, dataset_simp, dataset, site, block, plot, variables) %>%
+    dplyr::summarize(mean = mean(vals, na.rm = T),
+                     std.dev = sd(vals, na.rm = T),
+                     sample.size = dplyr::n(),
+                     std.error = std.dev / sqrt(sample.size)) %>%
+    dplyr::ungroup()
+  
+  # Next, average across plots within blocks
+  blocks_v1 <- plots_v1 %>%
+    # Rename summary metrics columns
+    dplyr::rename(prev_std.dev = std.dev,
+                  prev_sample.size = sample.size,
+                  prev_std.error = std.error,
+                  vals = mean) %>%
+    # Group by everything *except* plot
+    dplyr::group_by(lter, dataset_simp, dataset, site, block, variables) %>%
+    # And get averages (and variation metrics) again
+    dplyr::summarize(sample.size = dplyr::n(),
+                     mean = ifelse(test = all(sample.size) == 1,
+                                   yes = unique(vals),
+                                   no = mean(vals, na.rm = T)),
+                     std.dev = ifelse(test = all(sample.size) == 1,
+                                      yes = unique(prev_std.dev),
+                                      no = sd(vals, na.rm = T)),
+                     std.error = ifelse(test = all(sample.size) == 1,
+                                        yes = unique(prev_std.error),
+                                        no = std.dev / sqrt(sample.size)) ) %>%
+    # And ungroup
+    dplyr::ungroup()
+  
+  # Finally, average across blocks within sites
+  sites_v1 <- blocks_v1 %>%
+    # Rename summary metrics columns
+    dplyr::rename(prev_std.dev = std.dev, prev_sample.size = sample.size,
+                  prev_std.error = std.error, vals = mean) %>%
+    # Group by everything *except* block
+    dplyr::group_by(lter, dataset_simp, dataset, site, variables) %>%
+    # And get averages (and variation metrics) again
+    dplyr::summarize(sample.size = dplyr::n(),
+                     mean = ifelse(test = all(sample.size) == 1,
+                                   yes = unique(vals),
+                                   no = mean(vals, na.rm = T)),
+                     std.dev = ifelse(test = all(sample.size) == 1,
+                                      yes = unique(prev_std.dev),
+                                      no = sd(vals, na.rm = T)),
+                     std.error = ifelse(test = all(sample.size) == 1,
+                                        yes = unique(prev_std.error),
+                                        no = std.dev / sqrt(sample.size)) ) %>%
+    # And ungroup
+    dplyr::ungroup()
+  
+  # Tweak 'shape' of plot and site averages for viz/stats use
+  plots_v2 <- plots_v1 %>%
+    # Drop sample size column
+    dplyr::select(-sample.size) %>%
+    # Pivot remaining columns into long format
+    tidyr::pivot_longer(cols = mean:std.error,
+                        names_to = "stat", values_to = "value") %>%
+    # Combine statistic with variable
+    dplyr::mutate(name_actual = paste0(stat, "_", variables)) %>%
+    # Drop now-superseded columns
+    dplyr::select(-stat, -variables) %>%
+    # Flip back to wide format
+    tidyr::pivot_wider(names_from = name_actual,
+                       values_from = value)
+  
+  # Do the same for the site-level averages
+  sites_v2 <- sites_v1 %>%
+    # Drop sample size column
+    dplyr::select(-sample.size) %>%
+    # Pivot remaining columns into long format
+    tidyr::pivot_longer(cols = mean:std.error,
+                        names_to = "stat", values_to = "value") %>%
+    # Combine statistic with variable
+    dplyr::mutate(name_actual = paste0(stat, "_", variables)) %>%
+    # Drop now-superseded columns
+    dplyr::select(-stat, -variables) %>%
+    # Flip back to wide format
+    tidyr::pivot_wider(names_from = name_actual,
+                       values_from = value)
+  
+  # Get file names for both plot-level and site-level averages
+  site_name <- gsub(pattern = "_stats-ready_", replacement = "_site-avgs_", x = focal_ready)
+  plot_name <- gsub(pattern = "_stats-ready_", replacement = "_plot-avgs_", x = focal_ready)
+  
+  # Export both locally
+  write.csv(x = sites_v2, na = '', row.names = F,
+            file = file.path("data", "averages", site_name))
+  write.csv(x = plots_v2, na = '', row.names = F,
+            file = file.path("data", "averages", plot_name))
+  
+} # Closing loop
 
 
 
-
-## ------------------------------------------ ##
-# Spatial Aggregation ----
-## ------------------------------------------ ##
-
-# We also want to average data within progressively coarser units of spatial organization
-## First, average across cores within plots
-## Second, average across plots within blocks
-## Third, average across blocks within sites
-
-# Prepare to take averages by:
-avgs_prep <- sparc_stats %>%
-  # Dropping core-specific depth/horizon columns
-  dplyr::select(-core, -dplyr::starts_with("horizon"),
-                -dplyr::starts_with("depth."), -core.length_cm,
-                -bulk.density_g.cm3) %>%
-  # Flipping to long format
-  tidyr::pivot_longer(cols = slow.P_conc_mg.kg:N_conc_percent,
-                      names_to = "variables", values_to = "vals")
-
-# Check structure
-dplyr::glimpse(avgs_prep)
 
 # Begin with averaging across cores within plots
 plots_v1 <- avgs_prep %>%
